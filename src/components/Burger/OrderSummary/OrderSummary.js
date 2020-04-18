520acd3c87d638a9c7650bd44c42f18c7d3cfe75
sujan <sujanpoojary884@gmail.com>
2020-04-18 19:49:21 +0530

    modal updated

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Aux from '../../../hoc/Auxiliary';
 const orderSummary = (props) => {
-  const ingredientSummary = props.ingredients;
+  const ingredientSummary = Object.keys(props.ingredients).map((itemKey) => {
+    return (
+      <li key={itemKey}>
+        <span style={{ textTransform: 'capitalize' }}>{itemKey}</span>:{' '}
+        {props.ingredients[itemKey]}
+      </li>
+    );
+  });
   return (
     <Aux>
-      <h3>Your order</h3>
+      <h3>Your Orders</h3>
       <p>A delicious burger with the following ingredients</p>
-      <ul></ul>
+      <ul>{ingredientSummary}</ul>
+      <p>Continue to Checkout?</p>
     </Aux>
   );
 };
