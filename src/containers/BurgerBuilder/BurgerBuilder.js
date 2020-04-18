ecb6c2dd03bbeac2c35ebcb0ffabacb20bca8e42
sujan <sujanpoojary884@gmail.com>
2020-04-18 13:51:31 +0530

    state component build

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 0,
+      meat: 1,
+    },
+  };
   render() {
     return (
       <Aux>
-        <div>Burger</div>
-        <div>Build controls</div>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls />
       </Aux>
     );
   }
